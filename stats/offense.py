ea89d7238f1ffe0d8a01cce31777821de499a0fd
MDRODGERS17 <marissa.rodgers@ibm.com>
2020-03-16 17:09:22 -0500

    Completed module

--- stats/offense.py
+++ stats/offense.py
@@ -4,12 +4,24 @@ import matplotlib.pyplot as plt
 from data import games
 
 plays = games[games['type'] == 'play']
+plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
 
-strike_outs = plays[plays['event'].str.contains('K')]
-strike_outs = strike_outs.groupby(['year', 'game_id']).size()
-strike_outs = strike_outs.reset_index(name='strike_outs')
-strike_outs = strike_outs.loc[:, ['year', 'strike_outs']].apply(pd.to_numeric)
+hits = plays.loc[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)'), ['inning', 'event']]
+hits.loc[:, 'inning'] = pd.to_numeric(hits.loc[:, 'inning'])
 
-strike_outs.plot( y='strike_outs', x='year', kind='scatter').legend(['Strike Outs'])
+replacements = {
+  r'^S(.*)': 'single',
+  r'^D(.*)': 'double',
+  r'^T(.*)': 'triple',
+  r'^HR(.*)': 'hr'
+}
+
+hit_type = hits['event'].replace(replacements, regex=True)
+hits = hits.assign(hit_type=hit_type)
+hits = hits.groupby(['inning', 'hit_type']).size().reset_index(name='count')
+hits['hit_type'] = pd.Categorical(hits['hit_type'], ['single', 'double', 'triple', 'hr'])
+hits = hits.sort_values(['inning', 'hit_type'])
+hits = hits.pivot(index='inning', columns='hit_type', values='count')
+hits.plot.bar(stacked=True)
 
 plt.show()
